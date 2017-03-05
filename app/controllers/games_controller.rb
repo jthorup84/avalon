2366f8ff2a074905967b7723c5fd57b3c2c1bc57
Jeff <jthorup84@gmail.com>
2017-03-04 18:39:34 -0700

    fixed a bug

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,7 +2,7 @@ class GamesController < ApplicationController
   def update
     @game = Game.find params[:id]
     @game.update(game_params)
-    GameBuilder.new(@game).assign_random_roles
+    ::GameBuilder.new(@game).assign_random_roles
     redirect_to :back
   end
 
