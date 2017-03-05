9b05c200476dd55f5a2d69bcaade08d2e159f3cf
Jeff <jthorup84@gmail.com>
2017-03-04 18:51:44 -0700

    fixed another bug

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Avalon
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.autoload_paths += %W(#{config.root}/app/models/characters)
-    config.autoload_paths += %W(#{config.root}/lib)
+    config.eager_load_paths += %W(#{config.root}/app/models/characters)
+    config.eager_load_paths += %W(#{config.root}/lib)
   end
 end
